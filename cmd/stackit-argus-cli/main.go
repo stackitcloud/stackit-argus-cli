3b45d2502e955138111a12003c3d66e511393795
Steffen Exler <Steffen.Exler@mail.schwarz>
2022-07-28 08:38:37 +0200

    Add basic CLI
    Signed-off-by: Steffen Exler <Steffen.Exler@mail.schwarz>

--- cmd/stackit-argus-cli/main.go
+++ cmd/stackit-argus-cli/main.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/stackitcloud/stackit-argus-cli/cmd/stackit-argus-cli/cmd"
 	"github.com/stackitcloud/stackit-argus-cli/internal/log"
 	"go.uber.org/zap"
-
-	// This controls the maxprocs environment variable in container runtimes.
-	// see https://martin.baillie.id/wrote/gotchas-in-the-go-network-packages-defaults/#bonus-gomaxprocs-containers-and-the-cfs
-	_ "go.uber.org/automaxprocs"
 )
 
 func main() {
@@ -28,6 +25,8 @@ func run() error {
 	defer logger.Sync()
 
 	logger.Info("Hello world!", zap.String("location", "world"))
+
+	cmd.Execute()
 
 	return nil
 }
