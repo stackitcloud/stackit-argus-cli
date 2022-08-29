d7ae7b1a8db703daf88fe739aa375a002cf022b1
Christopher Paul <christopher.paul@mail.schwarz>
2022-08-29 16:12:11 +0200

    removed unnecessary parser file and moved http reqeust code into api file

--- cmd/stackit-argus-cli/cmd/auth.go
+++ cmd/stackit-argus-cli/cmd/auth.go
@@ -4,20 +4,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"encoding/json"
 	"github.com/spf13/cobra"
-	"io"
-	"net/http"
+	"github.com/stackitcloud/stackit-argus-cli/cmd/stackit-argus-cli/rest"
 )
-
-type serviceAccount struct {
-	id string `json:"id"`
-}
-
-type idToken struct {
-	token      string `json:"token"`
-	validUntil string `json:"validUntil"`
-}
 
 // authCmd represents the auth command
 var authCmd = &cobra.Command{
@@ -31,91 +20,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		client := &http.Client{}
-
-		// Create a service account via API. Use your own bearer token for creating it
-		req, err := http.NewRequest("POST",
-			"https://api-dev.stackit.cloud/service-account/v2/projects/"+args[0]+"/service-accounts",
-			nil)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		req.Header.Set("Authorization", "")
-
-		res, err := client.Do(req)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-
-		if res.StatusCode != http.StatusCreated {
-			res.Body.Close()
-			println("Status is: ", res.Status)
-			return
-		}
-		serviceAccount := &serviceAccount{}
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		res.Body.Close()
-		if err = json.Unmarshal(body, &serviceAccount); err != nil {
-			println("error is - ", err.Error())
-		}
-
-		// Add the service account to the project with a role
-		req, err = http.NewRequest("POST",
-			"https://api-dev.stackit.cloud/membership/v1/projects/"+args[0]+"/roles/project.member/service-accounts",
-			nil)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		req.Header.Set("Authorization", "")
-		req.Form.Set("serviceAccountId", serviceAccount.id)
-		res, err = client.Do(req)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		res.Body.Close()
-
-		// Create an id token
-		req, err = http.NewRequest("POST",
-			"https://api-dev.stackit.cloud/service-account/v1/projects/"+args[0]+"/service-accounts/"+serviceAccount.id+"/access-tokens",
-			nil)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		req.Header.Set("Authorization", "")
-		req.Form.Set("ttlDays", "180")
-		res, err = client.Do(req)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		if res.StatusCode != http.StatusCreated {
-			res.Body.Close()
-			println("Status is: ", res.Status)
-			return
-		}
-
-		idToken := &idToken{}
-		body, err = io.ReadAll(res.Body)
-		if err != nil {
-			print("error is - ", err.Error())
-			return
-		}
-		res.Body.Close()
-		if err = json.Unmarshal(body, &idToken); err != nil {
-			println("error is - ", err.Error())
-		}
-
-		println("Here is your auth token:\n", idToken.token)
-		println("It will expire on: ", idToken.validUntil)
+		rest.Authorize(args[0])
 	},
 }
 
--- cmd/stackit-argus-cli/rest/api_client.go
+++ cmd/stackit-argus-cli/rest/api_client.go
@@ -1,1 +1,104 @@
 package rest
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type serviceAccount struct {
+	Id string `json:"id"`
+}
+
+type idToken struct {
+	Token      string `json:"token"`
+	ValidUntil string `json:"validUntil"`
+}
+
+func Authorize(projectId string) {
+	client := &http.Client{}
+
+	// Create a service account via API. Use your own bearer token for creating it
+	req, err := http.NewRequest("POST",
+		"https://api-dev.stackit.cloud/service-account/v2/projects/"+projectId+"/service-accounts",
+		nil)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	req.Header.Set("Authorization", "") //TODO: read Token for Authorization Header from Config file
+
+	res, err := client.Do(req)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		res.Body.Close()
+		println("Status is: ", res.Status)
+		return
+	}
+	serviceAccount := &serviceAccount{}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	res.Body.Close()
+	if err = json.Unmarshal(body, &serviceAccount); err != nil {
+		println("error is - ", err.Error())
+	}
+
+	// Add the service account to the project with a role
+	req, err = http.NewRequest("POST",
+		"https://api-dev.stackit.cloud/membership/v1/projects/"+projectId+"/roles/project.member/service-accounts",
+		nil)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	req.Header.Set("Authorization", "")
+	req.Form.Set("serviceAccountId", serviceAccount.Id)
+	res, err = client.Do(req)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	res.Body.Close()
+
+	// Create an id token
+	req, err = http.NewRequest("POST",
+		"https://api-dev.stackit.cloud/service-account/v1/projects/"+projectId+"/service-accounts/"+serviceAccount.Id+"/access-tokens",
+		nil)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	req.Header.Set("Authorization", "")
+	req.Form.Set("ttlDays", "180")
+	res, err = client.Do(req)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	if res.StatusCode != http.StatusCreated {
+		res.Body.Close()
+		println("Status is: ", res.Status)
+		return
+	}
+
+	idToken := &idToken{}
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		print("error is - ", err.Error())
+		return
+	}
+	res.Body.Close()
+	if err = json.Unmarshal(body, &idToken); err != nil {
+		println("error is - ", err.Error())
+	}
+
+	println("Here is your auth token:\n", idToken.Token)
+	println("It will expire on: ", idToken.ValidUntil)
+}
