decfc1cf31060550e8d36de8187fcd27997f4192
akliek <www.klekanton@gmail.com>
2022-10-24 15:26:56 +0200

    config file validated

--- cmd/stackit-argus-cli/cmd/root.go
+++ cmd/stackit-argus-cli/cmd/root.go
@@ -1,20 +1,20 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	logging "github.com/stackitcloud/stackit-argus-cli/internal/log"
-	"os"
-
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	logging "github.com/stackitcloud/stackit-argus-cli/internal/log"
+	"os"
 )
 
-var confFile string
-var instanceId string
-var projectId string
-var debugMode bool
+// variables to save flags' values
+var (
+	confFile   string
+	instanceId string
+	projectId  string
+	debugMode  bool
+)
 
 var logger = logging.New()
 
@@ -29,55 +29,79 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() error {
-	err := rootCmd.Execute()
-	if err != nil {
-		logger.Error("an error occurred", logging.String("err", err.Error()))
-
+	if err := rootCmd.Execute(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func NewArgusCliCmd() *cobra.Command {
-	return rootCmd
-}
-
+// init commands and flags for the root command
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	rootCmd.AddCommand(getCmd)
 	rootCmd.AddCommand(createCmd)
 	rootCmd.AddCommand(updateCmd)
 	rootCmd.AddCommand(deleteCmd)
 
-	rootCmd.PersistentFlags().StringVarP(&confFile, "config", "c", "./.stackit-argus-cli.yaml", "provide config file (default is ./.stackit-argus-cli.yaml)")
-	rootCmd.PersistentFlags().StringVarP(&instanceId, "instanceId", "i", "", "provide instance id that should be used")
-	rootCmd.PersistentFlags().StringVarP(&projectId, "projectId", "p", "", "provide project id that should be used")
-	rootCmd.PersistentFlags().BoolVarP(&debugMode, "debug", "d", false, "turn on debug mode to see more information about the command")
+	rootCmd.PersistentFlags().StringVarP(&confFile, "config", "c", "./.stackit-argus-cli.yaml",
+		"provide config file (default is ./.stackit-argus-cli.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&instanceId, "instanceId", "i", "",
+		"provide instance id that should be used")
+	rootCmd.PersistentFlags().StringVarP(&projectId, "projectId", "p", "",
+		"provide project id that should be used")
+	rootCmd.PersistentFlags().BoolVarP(&debugMode, "debug", "d", false,
+		"turn on debug mode to see more information about the command")
+
+	cobra.OnInitialize(initConfig)
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if confFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(confFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	viper.SetConfigFile(confFile)
 
-		// Search config in home directory with name ".stackit-argus-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.AddConfigPath(".")
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".stackit-argus-cli")
+	// read config file
+	err := viper.ReadInConfig()
+	cobra.CheckErr(err)
+
+	// get instance id from a config file, if it is not set via flag
+	if instanceId == "" {
+		instanceId = viper.GetString("current_instance")
+		if instanceId == "" {
+			fmt.Println("Please, set a current instance id in a config file. Default config file path is" +
+				"./.stackit-argus-cli.yaml. Current instance id key is current_instance.")
+
+			os.Exit(1)
+		}
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	// get project id from a config file, if it is not set via flag
+	if projectId == "" {
+		projectId = viper.GetString("current_project")
+		if projectId == "" {
+			fmt.Println("Please, set a current project id in a config file. Default config file path is" +
+				"./.stackit-argus-cli.yaml. Current project id key is current_project.")
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		logger.Error("cannot read config file", logging.String("err", err.Error()))
+			os.Exit(1)
+		}
 	}
+}
+
+// NewArgusCliCmd returns root cmd
+func NewArgusCliCmd() *cobra.Command {
+	return rootCmd
+}
+
+// GetInstanceId gets instance id from flag if set, otherwise gets instance id from the config file
+func GetInstanceId() string {
+	return instanceId
+}
+
+// GetProjectId gets project id from flag if set, otherwise gets project id from the config file
+func GetProjectId() string {
+	return projectId
+}
+
+// IsDebugMode checks if debug mode is set
+func IsDebugMode() bool {
+	return debugMode
 }
